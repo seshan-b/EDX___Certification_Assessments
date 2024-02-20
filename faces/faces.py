7d609b6ddcd59ccc582d8b78f02a1e7e3d835c1a
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-02-20 03:33:50 +0000

    Tue, Feb 20, 2024, 4:33 PM +13:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,5 +1,5 @@
 # Get input from the user
-user_input = input("Enter a text with emoticons: ")
+user_input = input("")
 
 # Use the print function with the sep argument to replace emoticons with emojis
 print(*user_input.replace(":)", "😊").replace(":(", "😞"), sep="")

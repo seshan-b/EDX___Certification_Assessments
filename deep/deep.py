181c6d243556d63a10704d4cc851b2e0babd7fa0
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-02-22 03:56:28 +0000

    Thu, Feb 22, 2024, 4:56 PM +13:00

---
+++ deep/deep.py
@@ -0,0 +1,10 @@
+def check_great_question_answer(answer):
+    return_answer answer = answer.lower()
+
+    if answer_lower == "42" or "forty-two" in answer_lower or "forty two" in answer_lower:
+        return "Yes"
+    else:
+        return "No"
+
+
+def main():

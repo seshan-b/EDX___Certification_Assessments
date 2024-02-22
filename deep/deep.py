219fd5d6b9a641aab957c6c724d9fcd5d18cec3f
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-02-22 04:05:02 +0000

    Thu, Feb 22, 2024, 5:05 PM +13:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,7 +1,7 @@
 def check_great_question_answer(answer):
 
 
-    return_answer answer = answer.lower() # Convert to lower case
+    return_answer = answer.lower() # Convert to lower case
 
     if return_answer == "42" or "forty-two" in return_answer or "forty two" in return_answer:
         return "Yes"
@@ -13,6 +13,6 @@ def main():
 
     user_answer = input("What is the answer to the Great Question of Life, the Universe, and Everything? ")
 
-    # Check the user's answer 
+    # Check the user's answer
     result = check_great_question_answer(user_answer)
     print(f"Is the answer correct? {result}")

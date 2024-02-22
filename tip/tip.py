551bf4555aebdf81c21db55ed9c93204de55f55d
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-02-22 03:25:07 +0000

    Thu, Feb 22, 2024, 4:25 PM +13:00

--- tip/tip.py
+++ tip/tip.py
@@ -1,1 +1,20 @@
-l
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(d):
+
+
+    return d
+
+
+def percent_to_float(p):
+
+
+    return p
+
+
+main()

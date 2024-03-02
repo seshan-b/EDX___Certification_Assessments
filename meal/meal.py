5eb36dc89a837e91f4b651b01c4ce1e51d893cf8
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-03-02 08:48:06 +0000

    Sat, Mar 2, 2024, 9:48 PM +13:00

--- meal/meal.py
+++ meal/meal.py
@@ -3,21 +3,28 @@
 # Convert time to 24 hours
 def convert_to_24_hours(time_24hr):
 
-    # Check for meal times
-    if "07:00" <= time_24hr <= "08:00":
-        meal = "Breakfast"
-    elif "12:00" <= time_24hr <= "13:00":
-        meal = "Lunch"
-    elif "20:00" <= time_24hr <= "21:00":
-        meal = "Dinner"
 
     # Assuming input format like "HH:mm"
     return time_24hr
 
 
-time_24hr = "07:00"
+
+time_24hr = "12:01"
+
 result = convert_to_24_hours(time_24hr)
-print(result)
+
+
+# Check for meal times
+if "07:00" <= time_24hr <= "08:00":
+    meal = "Breakfast"
+elif "12:00" <= time_24hr <= "13:00":
+    meal = "Lunch"
+elif "20:00" <= time_24hr <= "21:00":
+    meal = "Dinner"
+else:
+    meal = "No time given"
+
+print(meal)
 
 
 

f545e0bd687d6f99f8e4f32a64094c321f4c796b
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-03-02 10:10:26 +0000

    Sat, Mar 2, 2024, 11:10 PM +13:00

--- meal/meal.py
+++ meal/meal.py
@@ -17,11 +17,11 @@ def main():
 
     # Check for meal times
     if 7.0 <= time_in_24hr <= 8.0:
-        meal_type = "breakfast time"
+        meal_type = "breakfast time..."
     elif 12.0 <= time_in_24hr <= 13.0:
-        meal_type = "lunch time"
+        meal_type = "lunch time..."
     elif 20.0 <= time_in_24hr <= 21.0:
-        meal_type = "dinner time"
+        meal_type = "dinner time..."
     else:
         meal_type = "No time given"
 

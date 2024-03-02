47e6b143534dab170f56397cb94f540a67437107
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-03-02 03:13:51 +0000

    Sat, Mar 2, 2024, 4:13 PM +13:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,9 +1,17 @@
-
+import os
+import magic
 
 # Get the user input
 
 
 # Create a function to get the file media type
+def get_media_type(file_name):
+    file_path = os.path.abspath(file_name)
+    mime = magic.Magic()
+    media_type = mime.from_file(file_path)
+    return media_type
 
+
+print(get _media_type("image.jpg"))
 
 # output the file media type

964624545d43b51c6b078fbe01dab2a83b890f60
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-03-02 04:30:46 +0000

    Sat, Mar 2, 2024, 5:30 PM +13:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -3,6 +3,7 @@
 # Create a function for adding subtracting, multiplying and dividing.
 
 def perform_operations(a, b):
+    
     addition_result = a + b
     subtraction_result = a - b
     multiplication_result = a * b

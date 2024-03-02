7b3a3792b134a50640807cc7874dcd7169d1b485
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-03-02 10:46:36 +0000

    Sat, Mar 2, 2024, 11:46 PM +13:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,10 @@
 # Convert time to 24 hours
-def convert_to_24_hours(time_in_24hr):
+def convert(time):
     # Convert time to decimal representation
-    hours, minutes = map(int, time_in_24hr.split(':'))
+    hours, minutes = map(int, time.split(':'))
     time_decimal = round(hours + minutes / 60.0, 2)
 
-    print(time_decimal)
+    # print(time_decimal)
     return time_decimal
 
 
@@ -12,16 +12,16 @@ def main():
     # Get user input. Eg: 7:00, 8:00
     user_input = str(input("Get the time (Eg: 7:00): "))
 
-    time_in_24hr = convert_to_24_hours(user_input)
+    time = convert(user_input)
 
     # print(time_in_24hr)
 
     # Check for meal times
-    if 7.0 <= time_in_24hr < 8.0:
+    if 7.0 <= time <= 8.0:
         meal_type = "breakfast time"
-    elif 12.0 <= time_in_24hr < 13.0:
+    elif 12.0 <= time <= 13.0:
         meal_type = "lunch time"
-    elif 18.0 <= time_in_24hr < 19.0:
+    elif 18.0 <= time <= 19.0:
         meal_type = "dinner time"
     else:
         meal_type = "No time given"

d1e62728b5f11f0a0ca93f710f4e844a382d1a4d
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-03-02 04:13:59 +0000

    Sat, Mar 2, 2024, 5:13 PM +13:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -4,6 +4,9 @@ import mimetypes
 # Create a function to get the file media type
 def get_media_type(file_name):
 
+
+    file_name.strip().lower()
+
     # The underscore is to get 1st part of array
     media_type, _ = mimetypes.guess_type(file_name)
     return media_type
@@ -14,7 +17,7 @@ def get_media_type(file_name):
 def main():
 
     # Get the user input Clean data on input
-    user_input = str(input("Enter the file name: ")).strip().lower()
+    user_input = str(input("Enter the file name: "))
     if '.' in user_input:
 
         result = get_media_type(user_input)

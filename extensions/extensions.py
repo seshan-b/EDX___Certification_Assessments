674281648909327008bf0c9102fcf7ce3696d849
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-03-02 03:05:23 +0000

    Sat, Mar 2, 2024, 4:05 PM +13:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -13,9 +13,12 @@ if '.' in file_name:
 else:
     file_extension = None  # If there's no dot in the file name, set file_extension to None
 
+media_type = ''
+
 # Check if the file extension is in the list of supported suffixes
 if file_extension and file_extension in suffixes:
     # Use mimetypes to guess the media type
     media_type, _ = mimetypes.guess_type(file_name)
     # Print the guessed media type or a default if it couldn't be guessed
-print(f"The media type of the file is: {file_name}")
+    media_type
+print(f"The media type of the file is: {media_type}")

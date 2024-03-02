bb242c4a61de5329ad67826501fc169372c8eeb8
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-03-02 04:44:04 +0000

    Sat, Mar 2, 2024, 5:44 PM +13:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -8,19 +8,25 @@ def perform_operations(user_input):
     # Split the expression into numbers and operation
     x, operation, y = user_input.split()
 
+    # Convert numbers to float
+    num1 = float(num1)
+    num2 = float(num2)
+
     match operation:
-        case 'add':
+        case '+':
             return x + y
-        case 'subtract':
+        case '-':
             return x - y
-        case 'multiply':
+        case '*':
             return x * y
-        case 'divide' if y != 0:
+        case '/' if y != 0:
             return x / y
 
 
 
 def main():
+     user_input = input("Enter an expression (1 + 1): ")
+     return user_input
 
 
 main()

d5277b68a6356ed06c7b8f3f1b7dd6e2f4288496
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-03-02 09:33:40 +0000

    Sat, Mar 2, 2024, 10:33 PM +13:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,11 +1,11 @@
 
 
 # Convert time to 24 hours
-def convert_to_24_hours(time_24hr):
+def convert_to_24_hours(time_in_24hr):
 
     # Convert time to decimal representation
-    hours, minutes = map(int, time_24hr.split(':'))
-    time_decimal = hours + minutes / 60.0
+    hours, minutes = map(int, time_in_24hr.split(':'))
+    time_decimal = round(hours + minutes / 60.0)
 
     print(time_decimal)
     return time_decimal
@@ -15,18 +15,27 @@ time_24hr = "12:01"
 result = convert_to_24_hours(time_24hr)
 
 
-# Check for meal times
-if "07:00" <= time_24hr <= "08:00":
-    meal = "Breakfast"
-elif "12:00" <= time_24hr <= "13:00":
-    meal = "Lunch"
-elif "20:00" <= time_24hr <= "21:00":
-    meal = "Dinner"
-else:
-    meal = "No time given"
-print(time_24hr).round()
-print(meal)
 
 
 
-# Get user input. Eg: 7:00, 8:00
+
+def main:
+    # Get user input. Eg: 7:00, 8:00
+    user_input = str(input("Get the time (Eg: 7:00): "))
+
+    time_in_24hr = user_input
+
+    # Check for meal times
+    if "07:00" <= time_in_24hr <= "08:00":
+        meal_type = "Breakfast"
+    elif "12:00" <= time_in_24hr <= "13:00":
+        meal_type = "Lunch"
+    elif "20:00" <= time_in_24hr <= "21:00":
+        meal_type = "Dinner"
+    else:
+        meal_type = "No time given"
+    print(time_in_24hr)
+    print(meal_type)
+
+
+main()

9f6c821dcc184f0e662a1a93a437756170c60e7b
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-03-04 04:15:35 +0000

    Mon, Mar 4, 2024, 5:15 PM +13:00

---
+++ coke/coke.py
@@ -0,0 +1,8 @@
+# Implement a program that prompts the user to insert a coin, one at a time,
+
+# Info due amount to user each time.
+
+# Once the user has inputted at least 50 cents, output how many cents in change the user is owed.
+
+# Assume that the user will only input integers, and ignore any integer that isn’t an accepted denomination.
+

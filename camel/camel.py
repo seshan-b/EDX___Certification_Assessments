2eb1dda187f51624602720b00b86c2e835970414
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-03-04 03:42:17 +0000

    Mon, Mar 4, 2024, 4:42 PM +13:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,17 +1,13 @@
 def snake_case(input_string):
     # Use the replace method to replace spaces with underscores
-
-    for i in result_string:
-
-        result_string = input_string.replace(' ', '_').lower()
-        print(result, end="")
+    result_string = input_string.replace(' ', '_').lower()
     return result_string
 
 
 def main():
     user_input = str(input("Give a name: "))
 
-    process = snake_case(user_input)
+    process = snake_case(user_input, end="")
 
     print(process)
 

c647ce5d03da98301b6177baec1fb7096f60e0f5
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-04-26 23:54:32 +0000

    Sat, Apr 27, 2024, 11:54 AM +12:00

---
+++ mario-more/mario.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+// Prompt User
+int main(void)
+{
+   int height;
+    do
+    {
+        height = get_int("Height: ");  // Prompt the user for height
+    }
+    while (height < 1 || height > 8);  // Reprompt the user if they hit 0 or less than 1
+
+    // printf("The entered height is %d\n", height); // Hide this to pass test function
+    // Loop through each level of the pyramid
+    for (int row = 1; row <= height; row++)
+    {
+        // Print spaces before the hashes to align the pyramid to the right
+        for (int space = 0; space < height - row; space++)
+        {
+            printf(" ");  // Print a space
+        }
+        // Print the hashes that form the pyramid
+        for (int hash = 0; hash < row; hash++)
+        {
+            printf("#");  // Print a hash
+        }
+        // Print two spaces between the pyramids
+        printf("  ");
+
+        // Print the hashes that form the left-aligned pyramid
+        for (int hash = 0; hash < row; hash++)
+        {
+            printf("#");  // Print a hash
+        }
+
+        // Move to the next line after finishing a row
+        printf("\n");
+    }
+
+}
+
+
+

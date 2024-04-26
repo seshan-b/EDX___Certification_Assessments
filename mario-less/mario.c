4ffe617a81816a73db2265d2f0e76efe320f2bb7
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-04-26 23:31:14 +0000

    Sat, Apr 27, 2024, 11:31 AM +12:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -13,16 +13,20 @@ int main(void)
     while (height < 1 || height > 8);  // Reprompt the user if they hit 0 or less than 1
 
     printf("The entered height is %d\n", height);
+    // Loop through each level of the pyramid
     for (int row = 1; row <= height; row++)
     {
+        // Print spaces before the hashes to align the pyramid to the right
         for (int space = 0; space < height - row; space++)
         {
             printf(" ");  // Print a space
         }
+        // Print the hashes that form the pyramid
         for (int hash = 0; hash < row; hash++)
         {
             printf("#");  // Print a hash
         }
+         // Move to the next line after finishing a row
         printf("\n");
     }
 

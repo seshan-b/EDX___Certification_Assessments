2e868aae2db4e9265094671e931f0a5d4006e8f1
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-04-26 23:22:53 +0000

    Sat, Apr 27, 2024, 11:22 AM +12:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -3,11 +3,14 @@
 
 
 // Prompt User
-int main(void) {
+int main(void)
+{
    int height;
-    do {
+    do
+    {
         height = get_int("Height: ");  // Prompt the user for height
-    } while (height < 1 || height > 8);  // Reprompt the user if they hit 0 or less than 1
+    }
+    while (height < 1 || height > 8);  // Reprompt the user if they hit 0 or less than 1
 
     printf("The entered height is %d\n", height);
 

a71a3646adcf19c2644b8f15d921eeefa8d33a51
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-05-09 22:35:12 +0000

    Fri, May 10, 2024, 10:35 AM +12:00

---
+++ cash/cash.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+}
+
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt the user for change owed, in cents
+
+    // Calculate how many quarters you should give customer
+    // Subtract the value of those quarters from cents
+
+    // Calculate how many dimes you should give customer
+    // Subtract the value of those dimes from remaining cents
+
+    // Calculate how many nickels you should give customer
+    // Subtract the value of those nickels from remaining cents
+
+    // Calculate how many pennies you should give customer
+    // Subtract the value of those pennies from remaining cents
+
+    // Sum the number of quarters, dimes, nickels, and pennies used
+    // Print that sum
+}

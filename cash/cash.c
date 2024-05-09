385deedb85c3efc8b2bb609538cbd7cf6b0011a0
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-05-09 22:42:36 +0000

    Fri, May 10, 2024, 10:42 AM +12:00

--- cash/cash.c
+++ cash/cash.c
@@ -4,8 +4,16 @@
 int main(void)
 {
     // Prompt the user for change owed, in cents
+    int cents;
+    do
+    {
+        cents = get_int("Change owed (in cents): ");
+    }
+    while (cents < 0);
 
     // Calculate how many quarters you should give customer
+
+
     // Subtract the value of those quarters from cents
 
     // Calculate how many dimes you should give customer

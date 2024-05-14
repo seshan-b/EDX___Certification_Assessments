dd5a751f02286ff22e1d14624b670a8f26311deb
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-05-14 04:11:38 +0000

    Tue, May 14, 2024, 4:11 PM +12:00

--- credit/credit.c
+++ credit/credit.c
@@ -22,33 +22,31 @@ int main() {
 }
 
 // Calculate Checksum Check to see if input is or isn't 0
-// Multiply every other digit by 2, starting with secod-to-last digit
-// Add those products' digits together.
-// Add the sum to sum of the digits that wern't multiplied by 2
-
-// If the total's last digit is 0, number is valid!
-
 int calculate_checksum(long long card_number) {
+    // Multiply every other digit by 2, starting with secod-to-last digit
+    // Add those products' digits together.
+    // Add the sum to sum of the digits that wern't multiplied by 2
+    // If the total's last digit is 0, number is valid!
     int sum = 0;
-    int digit;
     int is_second = 0;
+    int digit;
 
     while (card_number > 0) {
         digit = card_number % 10;
         if (is_second) {
             digit *= 2;
-            if (digit > 9) {
-                digit -= 9;
-            }
+            // Add the digits of the product
+            sum += digit / 10;
+            sum += digit % 10;
+        } else {
+            sum += digit;
         }
-        sum += digit;
         card_number /= 10;
         is_second = !is_second;
     }
 
     return sum;
 }
-
 // Check Credit Card length and starting digits.
 
 // Print AMEX, MASTERCARD, VISA or INVALID CARD

5acdbb05ec36a87a28ee66090bc116364e580c5c
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-05-14 04:02:35 +0000

    Tue, May 14, 2024, 4:02 PM +12:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,6 +2,8 @@
 
 // Sudo Code
 
+
+
 // Prompt the user for input
 
 int main() {
@@ -11,10 +13,35 @@ int main() {
     printf("Enter your credit card number: ");
     scanf("%lld", &credit_card_number);
 
+    if (credit_card_number == 0) {
+        printf("INVALID CARD\n");
+        return 0;
+    }
+
     return 0;
 }
 
 // Calculate Checksum Check to see if input is or isn't 0
+int calculate_checksum(long long card_number) {
+    int sum = 0;
+    int digit;
+    int is_second = 0;
+
+    while (card_number > 0) {
+        digit = card_number % 10;
+        if (is_second) {
+            digit *= 2;
+            if (digit > 9) {
+                digit -= 9;
+            }
+        }
+        sum += digit;
+        card_number /= 10;
+        is_second = !is_second;
+    }
+
+    return sum;
+}
 
 // Check Credit Card length and starting digits.
 

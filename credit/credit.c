f4958496d5755f6d7545a760fc745afaee704da5
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-05-14 03:58:29 +0000

    Tue, May 14, 2024, 3:58 PM +12:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,8 +1,18 @@
-
+#include <stdio.h>
 
 // Sudo Code
 
 // Prompt the user for input
+
+int main() {
+    long long credit_card_number;
+
+    // Prompt the user for input
+    printf("Enter your credit card number: ");
+    scanf("%lld", &credit_card_number);
+
+    return 0;
+}
 
 // Calculate Checksum Check to see if input is or isn't 0
 

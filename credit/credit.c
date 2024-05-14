4c7151f5df94791dc6084292d6994d0ea591c652
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-05-14 03:54:37 +0000

    Tue, May 14, 2024, 3:54 PM +12:00

---
+++ credit/credit.c
@@ -0,0 +1,11 @@
+
+
+// Sudo Code
+
+// Prompt the user for input
+
+// Calculate Checksum Check to see if input is or isn't 0
+
+// Check Credit Card length and starting digits.
+
+// Print AMEX, MASTERCARD, VISA or INVALID CARD

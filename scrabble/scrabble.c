2699e5890cf93b1083e60b88e9c67c2be34e39aa
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-05-15 04:20:44 +0000

    Wed, May 15, 2024, 4:20 PM +12:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -6,4 +6,14 @@
 int main(void)
 {
 
+    // Use get_string from the cs50 library to get the first word from the user
+    string word1 = get_string("Enter the first word: ");
+    // Print the first word to check if it was correctly inputted (for debugging)
+    printf("First word: %s\n", word1);
+
+    // Use get_string from the cs50 library to get the second word from the user
+    string word2 = get_string("Enter the second word: ");
+    // Print the second word to check if it was correctly inputted (for debugging)
+    printf("Second word: %s\n", word2);
+
 }

e71c678ebea9dd9452d7c57dd66081b99e133257
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-07-08 05:24:26 +0000

    Mon, Jul 8, 2024, 5:24 PM +12:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -8,6 +8,31 @@
 
 
 # 1. Initialize a dictionary called `fruit_calories` with fruit names as keys and their calorie counts as values.
+def initialize_fruit_calories():
+    fruit_calories = {
+        'apple': 52,
+        'banana': 89,
+        'cherry': 50,
+        'date': 282,
+        'elderberry': 73,
+        'fig': 74,
+        'grape': 67,
+        'honeydew': 36,
+        'kiwi': 61,
+        'lemon': 29,
+        'mango': 60,
+        'nectarine': 44,
+        'orange': 47,
+        'papaya': 43,
+        'quince': 57,
+        'raspberry': 52,
+        'strawberry': 32,
+        'tangerine': 53,
+        'ugli fruit': 47,
+        'watermelon': 30
+    }
+    return fruit_calories
+
 
 # 2. Create a list called `fruits` containing the names of the fruits to check.
 

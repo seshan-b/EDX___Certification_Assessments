7031c66059edf479725300a24a6243268c4ce0b5
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-07-08 05:14:10 +0000

    Mon, Jul 8, 2024, 5:14 PM +12:00

---
+++ nutrition/nutrition.py
@@ -0,0 +1,6 @@
+
+# Use a conditional with 20 Boolean expressions
+# One for each fruit,
+# Use a dict to associate a fruit with its calories!
+# If k is a str and d is a dict, you can check whether k is a key in d with code like:
+# Eg: if k in d:

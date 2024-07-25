40a58042c3182219c314d4435b1c1fb2af72ede3
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-07-25 22:38:53 +0000

    Fri, Jul 26, 2024, 10:38 AM +12:00

---
+++ grocery/grocery.py
@@ -0,0 +1,6 @@
+
+### Breaking down the problem
+# Implement a program that prompts the user for items, one per line, until the user inputs control-d
+# Then output the user’s grocery list in all uppercase, sorted alphabetically by item.
+# Prefixing each line with the number of times the user inputted that item
+# No need to pluralize the items. Treat the user’s input case-insensitively.

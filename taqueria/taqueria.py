734063c3593230e011dcd3c5646e1689969c552f
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-07-25 22:10:03 +0000

    Fri, Jul 26, 2024, 10:10 AM +12:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -2,4 +2,4 @@
 # prompting them for items, one per line, until the user inputs control-d
 # After each inputted item, display the total cost of all items inputted thus far, prefixed with a dollar sign ($)
 # Treat the user’s input case insensitively. Ignore any input that isn’t an item.
-# Assume that every item on the menu will be titlecased.
+# Assume that every item on the menu will be title-cased.

30b2270b6b8162714c3c56e922d79c2fb8bf38ad
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-07-25 22:08:51 +0000

    Fri, Jul 26, 2024, 10:08 AM +12:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,2 +1,5 @@
 #  Implement a program that enables a user to place an order
-# prompting them for items, one per line, until the user inputs control-d 
+# prompting them for items, one per line, until the user inputs control-d
+# After each inputted item, display the total cost of all items inputted thus far, prefixed with a dollar sign ($)
+# Treat the user’s input case insensitively. Ignore any input that isn’t an item.
+# Assume that every item on the menu will be titlecased.

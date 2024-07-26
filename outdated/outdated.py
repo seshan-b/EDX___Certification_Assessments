a8f24d67f5f192890780fda4d23d0aae965e3302
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-07-26 01:11:08 +0000

    Fri, Jul 26, 2024, 1:11 PM +12:00

---
+++ outdated/outdated.py
@@ -0,0 +1,7 @@
+
+### Breaking down the problem
+# Implement a program that prompts the user for a date, AD (Anno Domini) in month-day-year order
+# It needs to be formatted like this 9/8/1636 or September 8, 1636
+# Wherein the month in the latter might be any of the values in the list below
+
+

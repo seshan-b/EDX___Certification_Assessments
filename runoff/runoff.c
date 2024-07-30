ad5292a615ab1e0346bb82bdf6d18299f4e1b06d
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-07-30 04:34:56 +0000

    Tue, Jul 30, 2024, 4:34 PM +12:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -9,7 +9,7 @@
 // Recall that preferences[i][j] stores the index of the candidate who is the jth ranked preference for the ith voter.
 
 
-// "tabulate" function
+// "tabulate" Function
 // The function should update the number of votes each candidate has at this stage in the runoff.
 // Recall that at each stage in the runoff, every voter effectively votes for their top-preferred candidate who has not already been eliminated.
 // Consider these hints:
@@ -19,6 +19,27 @@
 // Recall that the candidate struct has a field called votes, which you’ll likely want to update for each voter’s preferred candidate.
 // Recall that once you’ve cast a vote for a voter’s first non-eliminated candidate, you’ll want to stop there, not continue down their ballot. You can break out of a loop early using break inside of a conditional.
 
+// print_winner Function
+// If any candidate has more than half of the vote, their name should be printed and the function should return true.
+// If nobody has won the election yet, the function should return false.
+// Consider this hint:
+// Recall that voter_count stores the number of voters in the election. Given that, how would you express the number of votes needed to win the election?
+
+// find_min Function
+// The function should return the minimum vote total for any candidate who is still in the election.
+// Consider this hint:
+// You’ll likely want to loop through the candidates to find the one who is both still in the election and has the fewest number of votes. What information should you keep track of as you loop through the candidates?
+
+
+// "is_tie" Function
+// The function takes an argument min, which will be the minimum number of votes that anyone in the election currently has.
+// The function should return true if every candidate remaining in the election has the same number of votes, and should return false otherwise.
+// Consider this hint:
+// Recall that a tie happens if every candidate still in the election has the same number of votes. Note, too, that the is_tie function takes an argument min, which is the smallest number of votes any candidate currently has. How might you use min to determine if the election is a tie (or, conversely, not a tie)?
+
+// "eliminate" Function
+// The function takes an argument min, which will be the minimum number of votes that anyone in the election currently has.
+// The function should eliminate the candidate (or candidates) who have min number of votes.
 
 #include <cs50.h>
 #include <stdio.h>

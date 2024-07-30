596a566a4ececcdb15b4476d331023975792b9fc
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-07-30 03:50:56 +0000

    Tue, Jul 30, 2024, 3:50 PM +12:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -1,4 +1,5 @@
 // Breaking down the problem
+// Vote Function
 // Look for a Candidate called name.
 // If Candidate found, update their vote total and return true.
 // If no candidate found don't update any vote totals and return false

d925ee36e464c742e3a5bbf56eb0391a6c42f22f
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-08-12 05:59:04 +0000

    Mon, Aug 12, 2024, 5:59 PM +12:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,14 +1,8 @@
 # ### Breaking down the problem
-
-
-
-from pyfiglet import Figlet
-
-figlet = Figlet()
-
-figlet.getFonts()
-
-figlet.setFont(font=f)
-
-
-print(figlet.renderText(s))
+# Expects zero or two command-line arguments
+# Prompts the user for a str of text.
+# Zero if the user would like to output text in a random font.
+# Two if the user would like to output text in a specific font,
+# If this is the case the first of the two should be -f or --font, and the second of the two should be the name of the font.
+# The program should exit via sys.exit with an error message if first is not -f or --font or the second is not the name of a font.
+# Outputs that text in the desired font.

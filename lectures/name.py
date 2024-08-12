4f81e7a14df3372cba9309b216a3191de435defc
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-08-12 00:16:40 +0000

    Mon, Aug 12, 2024, 12:16 PM +12:00

--- lectures/name.py
+++ lectures/name.py
@@ -3,10 +3,7 @@ import sys
 # Check for errors
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
-elif len(sys.argv) > 2:
-    sys.exit("Too many arguments")
 
-
-
-# Print name tags
-print("Hello, my name is ", sys.argv[1])
+for arg in sys.argv:
+    # Print name tags
+    print("Hello, my name is ", sys.argv[1])

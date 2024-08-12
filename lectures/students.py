1c6e5a5603ad151fa2b74ae3b22b7f3777a98b54
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-08-12 05:03:18 +0000

    Mon, Aug 12, 2024, 5:03 PM +12:00

--- lectures/students.py
+++ lectures/students.py
@@ -1,3 +1,3 @@
-students = {"Hermione", "Gryffindor", "Harry", "Ron", "Draco", "Seshan"}
+students = {"Hermione":"Hermione":, "Gryffindor":"Gryffindor", "Harry":"Harry", "Ron":"Ron", "Draco":"Draco", "Seshan":"Seshan"}
 for student in students:
     print(student)

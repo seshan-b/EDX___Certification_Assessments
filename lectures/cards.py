988b075c3f06767a5ed5cc1af9d518a3567c19a6
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-08-12 05:13:30 +0000

    Mon, Aug 12, 2024, 5:13 PM +12:00

--- lectures/cards.py
+++ lectures/cards.py
@@ -3,7 +3,7 @@ import random
 cards = ["Jack", "Queen", "King"]
 
 def main():
-    print(random.choice(cards, weights=[75, 20, 5], k=2))
+    print(random.choices(cards, weights=[75, 20, 5], k=2))
 
 
 main()

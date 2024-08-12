5b2246e4fe1fe52a857b2dd7157ab3048dd51f64
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-08-12 00:07:47 +0000

    Mon, Aug 12, 2024, 12:07 PM +12:00

--- lectures/name.py
+++ lectures/name.py
@@ -5,5 +5,6 @@ if len(sys.argv) < 2:
     print("Too few arguments")
 elif len(sys.argv) > 2:
     print("Too many arguments")
-print("Hello, my name is ", sys.argv[1])
+else:
+    print("Hello, my name is ", sys.argv[1])
 

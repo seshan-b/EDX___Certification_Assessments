b04c0c623827258b748ce95685c3d18121c31062
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-08-12 00:06:59 +0000

    Mon, Aug 12, 2024, 12:06 PM +12:00

--- lectures/name.py
+++ lectures/name.py
@@ -1,3 +1,9 @@
 import sys
 
-print("Hello my name is", sys.argv[1])
+
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+print("Hello, my name is ", sys.argv[1])
+

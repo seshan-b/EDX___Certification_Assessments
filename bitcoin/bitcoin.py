61fa41331e58cbe3016ea6e384f555c5a9324475
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-08-19 23:39:44 +0000

    Tue, Aug 20, 2024, 11:39 AM +12:00

---
+++ bitcoin/bitcoin.py
@@ -0,0 +1,3 @@
+# ### Breaking down the problem
+# Expects the user to specify as a command-line argument the number of Bitcoins, that they would like to buy.
+# If that argument cannot be converted to a float, the program should exit via sys.exit with an error message.

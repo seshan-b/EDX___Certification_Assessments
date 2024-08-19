ff91cffb2b176e430040722cc9a511bdbf1c4f51
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-08-19 23:41:07 +0000

    Tue, Aug 20, 2024, 11:41 AM +12:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,3 +1,6 @@
 # ### Breaking down the problem
 # Expects the user to specify as a command-line argument the number of Bitcoins, that they would like to buy.
 # If that argument cannot be converted to a float, the program should exit via sys.exit with an error message.
+# Queries the API for the CoinDesk Bitcoin Price Index at https://api.coindesk.com/v1/bpi/currentprice.json,
+# Returns a JSON object, among whose nested keys is the current price
+# Bitcoin as a float Be sure to catch any exceptions

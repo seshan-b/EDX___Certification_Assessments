29fd98eaf66221ab13616dbdbb82babf895f8639
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-08-19 22:56:57 +0000

    Tue, Aug 20, 2024, 10:56 AM +12:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,6 +1,6 @@
 # ### Breaking down the problem
 # Prompts the user for names
-# one per line, until the user inputs control-d.
+# One per line, until the user inputs control-d.
 # Assume that the user will input at least one name
 # Bid adieu to those names,
-# separating two names with one and, three names with two commas and one and, and names with commas and one
+# Separating two names with one and, three names with two commas and one and, and names with commas and one

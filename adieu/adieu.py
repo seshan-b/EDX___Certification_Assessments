d58812a3fc7d5e8623b27afc5a935d2f04ce03ce
Seshan B <89568882+seshan-b@users.noreply.github.com>
2024-08-19 22:53:58 +0000

    Tue, Aug 20, 2024, 10:53 AM +12:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -3,3 +3,4 @@
 # one per line, until the user inputs control-d.
 # Assume that the user will input at least one name
 # Bid adieu to those names,
+# separating two names with one and, three names with two commas and one and, and names with commas and one
